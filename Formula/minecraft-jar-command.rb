db4cb8fd729fabc40f4e1e72fe3dac44557a3431
Ezekiel Elin <ezekielelin@me.com>
2021-02-07 16:31:54 -0500

    Reorganized formula

--- Formula/minecraft-jar-command.rb
+++ Formula/minecraft-jar-command.rb
@@ -3,17 +3,17 @@ class MinecraftJarCommand < Formula
   homepage "https://github.com/ezfe/minecraft-jar-command"
   head "https://github.com/ezfe/minecraft-jar-command.git"
 
-  depends_on :xcode => "12"
-
   stable do
     version "v2.0.2"
     url "https://github.com/ezfe/minecraft-jar-command/archive/#{version}.tar.gz"
     sha256 "333a9472d2fd648a2a5831c0d004f06051c42be1d56fc8c77eabf268ef7cc8e0"
   end
 
+  depends_on :xcode => "12"
+
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
-    system "mv", ".build/release/minecraft-jar-command", "minecraft-jar-command"
+    mv ".build/release/minecraft-jar-command", "minecraft-jar-command"
     bin.install "minecraft-jar-command"
   end
 end
