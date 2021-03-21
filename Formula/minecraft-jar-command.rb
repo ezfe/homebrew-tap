337789c0e94a7d4cc3a0f2313bcbf8108ea84fd4
Ezekiel Elin <ezekielelin@me.com>
2021-03-21 11:44:18 -0400

    Version 2.3.2

--- Formula/minecraft-jar-command.rb
+++ Formula/minecraft-jar-command.rb
@@ -4,9 +4,9 @@ class MinecraftJarCommand < Formula
   head "https://github.com/ezfe/minecraft-jar-command.git"
 
   stable do
-    version "v2.2.0"
+    version "v2.3.2"
     url "https://github.com/ezfe/minecraft-jar-command/archive/#{version}.tar.gz"
-    sha256 "faa3bebc11ad9e04a21caf6d525db5f1cdf0a3569069a06f0e21a45f340d30db"
+    sha256 "90d60e8c2cf2b8af5581caf146e51755c7e59dd03142a225f9fa4b64e8b56e95"
   end
 
   depends_on "openjdk"
